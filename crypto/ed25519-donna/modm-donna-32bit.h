19295966beafc78307206814c9a43de84bd7edee
ryan kurte <ryankurte@gmail.com>
2022-06-03 09:57:30 +1200

    continuing crypto replacement

--- crypto/ed25519-donna/modm-donna-32bit.h
+++ crypto/ed25519-donna/modm-donna-32bit.h
@@ -2,6 +2,9 @@
 	Public domain by Andrew M. <liquidsun@gmail.com>
 */
 
+#include <stdint.h>
+#include <stdlib.h>
+
 
 /*
 	Arithmetic modulo the group order n = 2^252 +  27742317777372353535851937790883648493 = 7237005577332262213973186563042994240857116359379907606001950938285454250989
